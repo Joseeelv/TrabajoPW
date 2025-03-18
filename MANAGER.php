672fffd6a8209a325d2dd8bb78fe0aaa16911210
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2025-03-18 22:37:14 +0100

    Inserts BBDD

---
+++ MANAGER.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Manager Dashboard</title>
+</head>
+<body>
+<h1>Manager Dashboard</h1>
+<?php
+// Manager dashboard
+// include 'header.php';
+// list all of the products and ingredients that hace stock
+
+?>
+</body>
+</html>
+
