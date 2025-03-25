77a55b556104570cb48ee8fe7d724a14e9ca2863
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2025-03-25 19:52:01 +0100

    Index

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Kebab</title>
+  <title>Döner Kebab Society</title>
+  <link rel="icon" href="./assets/images/logo/DKS.ico" type="image/x-icon">
   <link rel="stylesheet" href="./assets/css/styles.css">
   <link rel="stylesheet" href="./assets/css/carrusel.css">
   <script src="./assets/js/carrusel.js" defer></script>
@@ -13,7 +14,8 @@
 <body>
   <header>
     <nav class="navbar">
-      <h1>Bienvenido a KEBAB!</h1>
+      <h1>DÖNER KEBAB SOCIETY</h1>
+
       <a href="./index.php" class="menu-link">Inicio</a>
       <a href="./functions/menu.php" class="menu-link">Carta</a>
       <a href="./functions/contact.php" class="menu-link">Contacto</a>
@@ -28,9 +30,9 @@
       <button class="btn btn-register" onclick="window.location.href='./functions/register.php'">Unete a la
         sociedad</button>
       <div class="carousel">
-        <div class="slide"><img src="./assets/images/carrusel/first_slide.png" alt="Kebab 1"></div>
-        <div class="slide"><img src="./assets/images/carrusel/second_slide.png" alt="Kebab 2"></div>
-        <div class="slide"><img src="./assets/images/carrusel/third_slide.png" alt="Kebab 3"></div>
+        <div class="slide"><img src="./assets/images/carrusel/1.png" alt="Kebab 1"></div>
+        <div class="slide"><img src="./assets/images/carrusel/2.png" alt="Kebab 2"></div>
+        <div class="slide"><img src="./assets/images/carrusel/3.png" alt="Kebab 3"></div>
       </div>
       <button class="btn btn-left" onclick="prevSlide()">&#9664;</button>
       <button class="btn btn-right" onclick="nextSlide()">&#9654;</button>
